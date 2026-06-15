520c89534e0d95e4822591d27d4e25ab75711461
JorisBly <joris.bailly.2001@gmail.com>
2026-06-15 10:37:21 +0200

    refactor PokemonDetailsActivity to improve code organization and readability

--- app/src/main/java/com/example/pokedex_mobile/PokemonDetailsActivity.kt
+++ app/src/main/java/com/example/pokedex_mobile/PokemonDetailsActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.example.pokedex.Pokemon
 import com.example.pokedex_mobile.databinding.ActivityPokemonDetailsBinding
 import kotlinx.coroutines.launch
 
@@ -30,46 +31,15 @@ class PokemonDetailsActivity : AppCompatActivity() {
                 val secondType = if (pokemon?.types?.size == 2) pokemon.types[1].name else null
                 val elementColor = ElementsColors.valueOf(firstType)
 
-                binding.tvDetailType1.text = firstType
-                binding.tvDetailType1.chipBackgroundColor = ColorStateList.valueOf(elementColor.rgb)
+                updateTypes(firstType, elementColor, secondType)
 
-                if (secondType != null) {
-                    val secondElementColor = ElementsColors.valueOf(secondType)
-                    binding.tvDetailType2.visibility = View.VISIBLE
-                    binding.tvDetailType2.text = secondType
-                    binding.tvDetailType2.chipBackgroundColor = ColorStateList.valueOf(secondElementColor.rgb)
-                } else {
-                    binding.tvDetailType2.visibility = View.GONE
-                }
-
-                binding.tvDetailName.text = pokemon?.name?.fr
-                binding.tvDetailCategory.text = pokemon?.category
-
-                binding.tvDetailWeight.text = pokemon?.weight ?: ""
-                binding.tvDetailHeight.text = pokemon?.height ?: ""
-                binding.tvDetailGeneration.text = "Gén. ${pokemon?.generation ?: "Inconnu"}"
-
-                binding.pbHp.progress = pokemon?.stats?.hp ?: 0
-                binding.tvValueHp.text = pokemon?.stats?.hp?.toString() ?: ""
-                binding.pbAtk.progress = pokemon?.stats?.atk ?: 0
-                binding.tvValueAtk.text = pokemon?.stats?.atk?.toString() ?: ""
-                binding.pbDef.progress = pokemon?.stats?.def ?: 0
-                binding.tvValueDef.text = pokemon?.stats?.def?.toString() ?: ""
-                binding.pbSpeAtk.progress = pokemon?.stats?.speAtk ?: 0
-                binding.tvValueSpeAtk.text = pokemon?.stats?.speAtk?.toString() ?: ""
+                updatePokemonInfo(pokemon)
 
 
-                binding.pbHp.progressTintList = ColorStateList.valueOf(elementColor.rgb)
-                binding.pbAtk.progressTintList = ColorStateList.valueOf(elementColor.rgb)
-                binding.pbDef.progressTintList = ColorStateList.valueOf(elementColor.rgb)
-                binding.pbSpeAtk.progressTintList = ColorStateList.valueOf(elementColor.rgb)
+                updateProgressList(pokemon, elementColor)
 
-                binding.main.setBackgroundColor(elementColor.getLightTone(factor = 0.85f))
+                setBackground(elementColor)
 
-                binding.pbHp.progressBackgroundTintList = ColorStateList.valueOf(elementColor.getLightTone(factor = 0.5f))
-                binding.pbAtk.progressBackgroundTintList = ColorStateList.valueOf(elementColor.getLightTone(factor = 0.5f))
-                binding.pbDef.progressBackgroundTintList = ColorStateList.valueOf(elementColor.getLightTone(factor = 0.5f))
-                binding.pbSpeAtk.progressBackgroundTintList = ColorStateList.valueOf(elementColor.getLightTone(factor = 0.5f))
 
                 binding.tvDetailName.setTextColor(elementColor.getDarkTone(factor = 0.5f))
                 binding.tvDetailCategory.setTextColor(elementColor.getDarkTone(factor = 0.4f))
@@ -84,5 +54,59 @@ class PokemonDetailsActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    private fun updateTypes(
+        firstType: String,
+        elementColor: ElementsColors,
+        secondType: String?
+    ) {
+        binding.tvDetailType1.text = firstType
+        binding.tvDetailType1.chipBackgroundColor = ColorStateList.valueOf(elementColor.rgb)
+
+        if (secondType != null) {
+            val secondElementColor = ElementsColors.valueOf(secondType)
+            binding.tvDetailType2.visibility = View.VISIBLE
+            binding.tvDetailType2.text = secondType
+            binding.tvDetailType2.chipBackgroundColor =
+                ColorStateList.valueOf(secondElementColor.rgb)
+        } else {
+            binding.tvDetailType2.visibility = View.GONE
+        }
+    }
+
+    private fun updatePokemonInfo(pokemon: Pokemon?) {
+        binding.tvDetailName.text = pokemon?.name?.fr
+        binding.tvDetailCategory.text = pokemon?.category
+
+        binding.tvDetailWeight.text = pokemon?.weight ?: ""
+        binding.tvDetailHeight.text = pokemon?.height ?: ""
+        binding.tvDetailGeneration.text = "Gén. ${pokemon?.generation ?: "Inconnu"}"
+    }
+
+    private fun setBackground(elementColor: ElementsColors) {
+        binding.main.setBackgroundColor(elementColor.getLightTone(factor = 0.85f))
+    }
+
+    private fun updateProgressList(pokemon: Pokemon? ,elementColor: ElementsColors) {
+
+        binding.pbHp.progress = pokemon?.stats?.hp ?: 0
+        binding.tvValueHp.text = pokemon?.stats?.hp?.toString() ?: ""
+        binding.pbAtk.progress = pokemon?.stats?.atk ?: 0
+        binding.tvValueAtk.text = pokemon?.stats?.atk?.toString() ?: ""
+        binding.pbDef.progress = pokemon?.stats?.def ?: 0
+        binding.tvValueDef.text = pokemon?.stats?.def?.toString() ?: ""
+        binding.pbSpeAtk.progress = pokemon?.stats?.speAtk ?: 0
+        binding.tvValueSpeAtk.text = pokemon?.stats?.speAtk?.toString() ?: ""
+
+        binding.pbHp.progressTintList = ColorStateList.valueOf(elementColor.rgb)
+        binding.pbAtk.progressTintList = ColorStateList.valueOf(elementColor.rgb)
+        binding.pbDef.progressTintList = ColorStateList.valueOf(elementColor.rgb)
+        binding.pbSpeAtk.progressTintList = ColorStateList.valueOf(elementColor.rgb)
+
+        binding.pbHp.progressBackgroundTintList = ColorStateList.valueOf(elementColor.getLightTone(factor = 0.5f))
+        binding.pbAtk.progressBackgroundTintList = ColorStateList.valueOf(elementColor.getLightTone(factor = 0.5f))
+        binding.pbDef.progressBackgroundTintList = ColorStateList.valueOf(elementColor.getLightTone(factor = 0.5f))
+        binding.pbSpeAtk.progressBackgroundTintList = ColorStateList.valueOf(elementColor.getLightTone(factor = 0.5f))
     }
 }
